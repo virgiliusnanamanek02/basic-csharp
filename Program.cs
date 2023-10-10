d1c44569ebafdb3d7eb4465bc14b22c8f4776d18
vickynana19 <vickynana034@gmail.com>
2023-10-10 21:30:38 +0700

    variables and conditions

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int age = 23;
+        string name = "Nana";
+        double salary = 3000.50;
+        bool isStudent = true;
+
+        if(isStudent){
+            Console.WriteLine($"Hello, my name is {name} and I am {age} years old!");
+        }else{
+            Console.WriteLine($"My salary is {salary} per month!");
+        }
     }
 }
